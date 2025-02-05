9800febfca7d59481a05d09ca37b4dafd5c1f03b
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-06 00:00:53 +0300

    123

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 MODEL_PATH = "obesity-/model/model.pkl"  # Используйте прямые слэши
-IMAGE_PATH = "obesity-/imagine/imt-calculator.jpg"  # Используйте прямые слэши
+IMAGE_PATH = "obesity-/image/imt-calculator.jpg"  # Используйте прямые слэши
