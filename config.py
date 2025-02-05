1be822e28c1052b9541b345459c466e0e132af8d
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-05 20:33:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-MODEL_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\obesity-\\\model\\svc_model.pkl'
+MODEL_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\obesity-\\model\\svc_model.pkl'
 IMAGE_PATH = 'C:\\Users\\grimm\\Documents\\Stronghold_of_Digital_Martyrdom\\obesity-\\imt-calculator.jpg'
