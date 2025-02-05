2b1860e2303f7d7008bd59d08d00dd4d5ca62a13
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-06 01:01:54 +0300

    obesity-\model\RandomForestClassifier.pkl

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 import os
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "svc_model.pkl")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "RandomForestClassifier.pkl")
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image", "imt-calculator.jpg")
