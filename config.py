913609f5e1574549f7f55c7b3171e827e20132ca
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-05 23:48:13 +0300

    213

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-MODEL_PATH = 'obesity-\model\model.pkl'
-IMAGE_PATH = 'obesity-\imagine\imt-calculator.jpg'
+# config.py
+MODEL_PATH = "obesity-/model/model.pkl"  # Относительный путь к модели
+IMAGE_PATH = "obesity-/imagine/imt-calculator.jpg"  # Относительный путь к изображению
