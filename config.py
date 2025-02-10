44a51e941f0d7cfed47add3589dbcc81e5fc69e1
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-10 15:00:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 import os
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "top_model_95_percent.pkl")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "RandomForestClassifier.pkl")
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image", "imt-calculator.jpg")
