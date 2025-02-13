596fa94809007a9129ca62d81af8a9e56a62340c
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-14 02:00:29 +0300

    1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 import os
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "GBM_search.pkl")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "GBC_pipe.pkl")
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image", "imt-calculator.jpg")
