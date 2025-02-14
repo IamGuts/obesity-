a0e38b9dcd72b17e9d87153b885c227fa0deebfb
Ярослав Савик <femto_wing_of_darkness@mail.ru>
2025-02-14 20:01:20 +0300

    1

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ from obesity_predict import ObesityClassifier
 from config import MODEL_PATH, IMAGE_PATH
 import json
 import os 
-from pathlib import Path 
 
-
-# Загрузка описаний
-DESC_PATH = Path(__file__).resolve().parent / "text" / "descriptions.json"
-
-try:
-    with open(DESC_PATH, "r", encoding="utf-8") as f:
-        class_descriptions = json.load(f)
-except Exception as e:
-    st.error(f"Ошибка загрузки описаний: {str(e)}")
 
 
 def main():
@@ -77,17 +67,20 @@ def main():
             }
             encod_params_inv = {v: k for k, v in encod_params.items()}
 
-            if result is not None:
-                class_name = encod_params_inv[result[0]]
-                description = class_descriptions.get(class_name, "Описание не найдено")
-                st.success(f"**Результат:** {description}")
+            describe_result ={
+    'Normal_Weight' : 'Здоровый вес: ИМТ в диапазоне 18.5-24.9, оптимальное соотношение мышечной массы и жира.',
+    'Overweight_Level_I' : 'Предожирение: ИМТ 25-29.9, повышенная нагрузка на суставы, риск развития метаболических нарушений.',
+    'Overweight_Level_II' : 'Умеренный избыток веса: ИМТ 30-34.9 (часто классифицируется как ожирение I степени), начало жировых отложений в абдоминальной зоне.',
+    'Obesity_Type_I' : 'Ожирение I степени: ИМТ 35-39.9, повышенный риск диабета 2 типа и гипертонии.',
+    'Insufficient_Weight' : 'Дефицит массы: ИМТ <18.5, возможны проблемы с метаболизмом и иммунитетом.',
+    'Obesity_Type_II' : 'Ожирение II степени: ИМТ 40-44.9, выраженные жировые отложения, высокая нагрузка на сердечно-сосудистую систему.',
+    'Obesity_Type_III' : ' Морбидное ожирение: ИМТ ≥45, критический риск сопутствующих патологий (апноэ, артроз).'
+}
 
-                st.success(f'Результат предсказания: {result_text.[
-                    encod_params_inv[
-                        result[0]
-                        ]
-                    ]
-                    }')
+            if result is not None:
+                result_text = os.path.join(os.path.dirname(__file__), "text", "describe.py")
+
+                st.success(result[0])
             else:
                 st.error('Не удалось выполнить предсказание.')
         except Exception as e:
